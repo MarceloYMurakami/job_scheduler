b289464e6508a88c0ac39460f2f031d0b6ab93a3
Marcelo Murakami <marcelo.ymurakami@gmail.com>
2020-06-06 16:35:41 -0300

    Add gitignore

--- job_scheduler.c
+++ job_scheduler.c
@@ -3,25 +3,41 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <strings.h>
+#include <sys/stat.h>
+
+typedef struct Jobs
+{
+	int id;
+	char *desc;
+	char *data;
+	int time;
+} jobs;
+
+long int get_file_length(int fd)
+{
+	int l;
+	struct stat buf;
+
+	fstat(fd, &buf);
+	l = buf.st_size + 1;
+	return l;
+}
 
 int main ()
 {
 	int fd;
 	char *tmp;
-
+	jobs j1, j2, j3;
 
 	fd = open("./Base/base.json", O_RDONLY);
 	if (fd < 0)
-		printf("Error reading file\n");
-	else
 	{
-		tmp = (char *)malloc(100 * sizeof(char));
-		while (read (fd, tmp, 100 * sizeof(char)) != 0)
-		{
-			printf("%s\n", tmp);
-			bzero(tmp,100);
-		}
-		free(tmp);
+		printf("Error reading file\n");
+		return (0);
 	}
+	tmp = (char *)malloc(get_file_length(fd) * sizeof(char));
+	read (fd, tmp, get_file_length(fd) * sizeof(char));
+	printf("%s", tmp);
+	free(tmp);
 	return (0);
 }
